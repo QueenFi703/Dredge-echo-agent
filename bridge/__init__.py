29a99e8a3315b12d854425dbd7c338186a63260d
SophiaRose Cole <iosqueen86@gmail.com>
2026-09-10 09:56:54 -0500

    feat: export Dredge Echo research components

--- bridge/__init__.py
+++ bridge/__init__.py
@@ -1,9 +1,15 @@
 # Bridge package
 from .python_adapter import PythonAdapter
 from .llm_adapter import LLMAdapter, LLMBackendError
+from .search_adapter import TavilySearchAdapter, SearchBackendError
+from .research_agent import ResearchAgent, ResearchResult
 
 __all__ = [
     "PythonAdapter",
     "LLMAdapter",
     "LLMBackendError",
+    "TavilySearchAdapter",
+    "SearchBackendError",
+    "ResearchAgent",
+    "ResearchResult",
 ]
